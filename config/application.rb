18861dc0d88aa238e926bf30093ae633ad889756
sonyfe25cp <sonyfe25cp@gmail.com>
2013-05-01 23:45:34 +0800

    modify for deploy

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Omartech
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.initialize_on_precompile = false
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Omartech::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,13 +47,8 @@ Omartech::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( user.css
-                                  user/friends.css
-                                  user/index.css
-                                  user/posts.css
-                                  shared/bootstrap_and_overrides.css
-                                  common.css
+                                  shared/common.css
                                   index.js
-                                  nav_bar.js
                                   user.js
                                   new_posts.js
   )
